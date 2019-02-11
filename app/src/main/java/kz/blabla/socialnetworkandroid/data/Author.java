c003518db42a8daa679af5ff6ce54fd3e352e663
Yelnur Zhexenov <ejeksenov@gmail.com>
2019-02-11 17:04:16 +0600

    Post and Author models

---
+++ app/src/main/java/kz/blabla/socialnetworkandroid/data/Author.java
@@ -0,0 +1,41 @@
+package kz.blabla.socialnetworkandroid.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Author {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("username")
+    @Expose
+    private String username;
+
+
+    public Author() {
+    }
+
+    public Author(Integer id, String username) {
+        super();
+        this.id = id;
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
